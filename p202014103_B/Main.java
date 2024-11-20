783be46ec88aa246f68891408f08824e53c1d226
Seonjae Kim <theseonjae@gmail.com>
2024-11-20 09:45:12 +0900

    two

--- p202014103_B/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    // 자신의 학번과 이름을 입력하라
-    System.out.println("202014103 김선재");
-    System.out.println(Hello.sum(10));
-    System.out.println(Hello.sum(100));
-  }
-}
